8bf8c6f53a638d4111cbe105f32d45039ec05c87
Shayan <shayanramezani98@gmail.com>
2024-10-14 12:45:14 +0200

    functions added + igtignore

--- functions/GPregression.m
+++ functions/GPregression.m
@@ -1,20 +1,25 @@
-function[pred_mean, pred_cov] = GPregression(X,y,Xstar,hyp)
+function[GPmodel, predictions, mean, covariance_matrix, Kernel_train_data] = GPregression(X, y, Xstar, hyp)
+    N_X = size(X, 1);
+    N_Xstar = size(X, 1);
 
-    N = dim(X,1);
+    % fit GP model
+    GPmodel = fitrgp(X, y, 'KernelFunction', 'squaredexponential', 'KernelParameters', sqrt(hyp));
+    % Get fitted Kernel param values
+    [sigma_l, sigma_f] = GPmodel.KernelInformation.KernelParameters;
 
-    GPmodel = fitrgp(X,y,'KernelFunction','squaredexponential','KernelParameters',hyp); %Calculate the GP model
-    [l,sign] = GPmodel.KernelInformation.KernelParameters; %!!! Check if square is nececary !!!
+    % get return values
+    Kernel_train_data = kernel_matrix(X, sigma_l, sigma_f);
+    predictions = predict(GPmodel, Xstar);
 
-    K_matrix = zeros(N,N); %allocate for speed
+    joint_kernel_matrix = kernel_matrix([X; X_star], sigma_l, sigma_f);
 
-    for i = 1:N
-        for j =i:N
-            val = sign^2*exp(-(norm(X(i,:)-X(j,:),'fro')^2)/(2*l^2));
-            K_matrix(i,j) = val;
-            K_matrix(j,i) = val;
-        end
-    end
+    % getting different parts of the joint covariance matrix
+    K_X_X = joint_kernel_matrix(1:N_X, 1:N_X);
+    K_X_Xstar = joint_kernel_matrix(N_X + 1:N_X + N_Xstar);
+    K_Xstar_X = K_X_Xstar.';
+    K_Xstar_Xstar = joint_kernel_matrix(N_X + 1:N_X+N_Xstar, N_X + 1:N_X + N_Xstar);
 
-
-
+    % calculate mean vector and covariance matrix based on eq 2.19 from the book
+    mean = K_X_X \ K_Xstar_X .* predictions;
+    covariance_matrix = K_Xstar_Xstar - K_X_X \ K_Xstar_X * K_X_Xstar;
 end
---
+++ functions/kernel_matrix.m
@@ -0,0 +1,24 @@
+function [Kernel] = kernel_matrix(X, sigma_l, sigma_f)
+    % non-optimized version
+    % N = size(X, 1);
+    % Kernel = zeros(N, N);
+    % for i = 1:N
+    %     for j =i:N
+    %         val = sigma_f^2*exp(-0.5/sigma_l * (norm(X(i,:)-X(j,:),'fro')^2));
+    %         Kernel(i,j) = val;
+    %         Kernel(j,i) = val;
+    %     end
+    % end
+
+    % optimized version of the for loop
+    % permute X to [3,2,1] to make use of broadcasting when subtracting
+    X_permuted = permute(X, [3,2,1]);
+    % difference of each row with itself and every other row (in 3D)
+    Kernel = X_permuted - X;
+    % absolute value and summed over second dimension to obtain frobenius norm squared
+    Kernel = abs(Kernel);
+    Kernel = sum(Kernel, 2);
+    % rest of the squared exponential kernel function calculation
+    Kernel = sigma_f^2 .* exp(-0.5/sigma_l^2 .* Kernel);
+end
+
