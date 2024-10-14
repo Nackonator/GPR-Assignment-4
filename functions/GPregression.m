7fe633168d4bebaab932149a114dcfdc05ecd0f8
NicoVerbeek <nico.verbeek@aerodelft.nl>
2024-10-14 11:59:15 +0200

    Te

--- functions/GPregression.m
+++ functions/GPregression.m
@@ -1,3 +1,20 @@
 function[pred_mean, pred_cov] = GPregression(X,y,Xstar,hyp)
 
+    N = dim(X,1);
+
+    GPmodel = fitrgp(X,y,'KernelFunction','squaredexponential','KernelParameters',hyp); %Calculate the GP model
+    [l,sign] = GPmodel.KernelInformation.KernelParameters; %!!! Check if square is nececary !!!
+
+    K_matrix = zeros(N,N); %allocate for speed
+
+    for i = 1:N
+        for j =i:N
+            val = sign^2*exp(-(norm(X(i,:)-X(j,:),'fro')^2)/(2*l^2));
+            K_matrix(i,j) = val;
+            K_matrix(j,i) = val;
+        end
+    end
+
+
+
 end
