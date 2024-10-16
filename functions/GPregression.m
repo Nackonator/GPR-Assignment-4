b0a1b6e10a3c51ce418c95a1013c93517526c202
Nackonator <nico@brunices.nl>
2024-10-16 22:00:12 +0200

    -

--- functions/GPregression.m
+++ functions/GPregression.m
@@ -1,10 +1,17 @@
 function[mean, covariance_matrix, Kernel_train_data] = GPregression(X, y, Xstar, hyp)
     N_X = size(X, 1);
     N_Xstar = size(Xstar, 1);
+
+    % fit GP model
+    %GPmodel = fitrgp(X, y, 'KernelFunction', 'squaredexponential', 'KernelParameters', sqrt(hyp));
+    % Get fitted Kernel param values
+    %hyp2 = GPmodel.KernelInformation.KernelParameters;
+
     sigma_l = hyp(1); sigma_f = hyp(2);
 
     % get return values
     Kernel_train_data = kernel_matrix(X, sigma_l, sigma_f);
+    %predictions = predict(GPmodel, Xstar);
 
     joint_kernel_matrix = kernel_matrix([X; Xstar], sigma_l, sigma_f);
 
@@ -24,6 +31,5 @@ function[mean, covariance_matrix, Kernel_train_data] = GPregression(X, y, Xstar,
     %--+++====BACKSLASH OPERATOR ISNIET MOGELIJK OMDAT DE NUMBER OF ROWS NIET HETZELFDE ZIJN.====+++--
     K_X_X_inv = inv(K_X_X); %pre-calculate the inverse seeing as it is needed in both the mean and covariance. This is also a very intensive inverse to calculate (big matrix).
     mean = K_Xstar_X * K_X_X_inv * y;
-    predictions = mean; %Weet niet zeker of dit het geval is.
     covariance_matrix = K_Xstar_Xstar - K_Xstar_X*K_X_X_inv*K_X_Xstar;
 end
