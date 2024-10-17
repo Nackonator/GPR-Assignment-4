9b2a9ef6acd3cc5c9c6c98e6f7261e224161a4e1
NicoVerbeek <nico.verbeek@aerodelft.nl>
2024-10-17 17:46:33 +0200

    oogabooga

--- functions/kernel_matrix.m
+++ functions/kernel_matrix.m
@@ -12,18 +12,21 @@ function Kernel = kernel_matrix(X, sigma_l, sigma_f)
     % end
 
     % optimized version of the for loop
-    % N = size(X, 1);
-    % % permute X to [3,2,1] to make use of broadcasting when subtracting
-    % X_permuted = permute(X, [3,2,1]);
-    % % difference of each row with itself and every other row (in 3D)
-    % Kernel = X_permuted - X;
-    % % absolute value and summed over second dimension to obtain frobenius norm squared
-    % Kernel = abs(Kernel);
-    % Kernel = sum(Kernel, 2);
-    % % rest of the squared exponential kernel function calculation
-    % Kernel = squeeze(sigma_f^2 .* exp(-0.5/sigma_l^2 .* Kernel));
+    N = size(X, 1);
+    % permute X to [3,2,1] to make use of broadcasting when subtracting
+    X_permuted = permute(X, [3,2,1]);
+    % difference of each row with itself and every other row (in 3D)
+    Kernel = X_permuted - X;
+    % absolute value and summed over second dimension to obtain frobenius norm squared
+    Kernel = abs(Kernel);
+    Kernel = sum(Kernel, 2);
+    % rest of the squared exponential kernel function calculation
+    Kernel = squeeze(sigma_f^2 .* exp(-0.5/sigma_l^2 .* Kernel));
     
     %even new method
-    Kernel = sigma_f^2 .* exp(-0.5/sigma_l^2 .* pdist2(X, X).^2);
+    %Kernel2 = sigma_f^2 .* exp(-0.5/sigma_l^2 .* pdist2(X, X).^2);
+
+    %assert(sum(Kernel-Kernel2,'all') < 1e-9)
+
 end
 
