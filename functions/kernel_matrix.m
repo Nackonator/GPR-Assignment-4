c683ee0a851413da9ecb7d03f7cc88cc51eafd28
Nackonator <nico@brunices.nl>
2024-10-18 20:08:17 +0200

    f
    f

--- functions/kernel_matrix.m
+++ functions/kernel_matrix.m
@@ -2,13 +2,13 @@ function Kernel = kernel_matrix(X, sigma_l, sigma_f)
     % non-optimized version, Is correct (I think)
     N = size(X, 1);
     Kernel = zeros(N, N);
-    for i = 1:N
-        for j = i:N
-            val = sigma_f * exp(-(norm(X(i,:) - X(j,:))^2)/(2*sigma_l));
-            Kernel(i,j) = val;
-            Kernel(j,i) = val;
-        end
-    end
+    % for i = 1:N
+    %     for j = i:N
+    %         val = sigma_f * exp(-(norm(X(i,:) - X(j,:))^2)/(2*sigma_l));
+    %         Kernel(i,j) = val;
+    %         Kernel(j,i) = val;
+    %     end
+    % end
 
     % % optimized version of the for loop
     % N = size(X, 1);
@@ -23,7 +23,7 @@ function Kernel = kernel_matrix(X, sigma_l, sigma_f)
     % Kernel = squeeze(sigma_f .* exp(-0.5/sigma_l .* Kernel));
     
     %even new method
-    %Kernel2 = sigma_f^2 .* exp(-0.5/sigma_l^2 .* pdist2(X, X).^2);
+    Kernel = sigma_f .* exp(-0.5/sigma_l .* pdist2(X, X).^2);
 
     %assert(sum(Kernel-Kernel2,'all') < 1e-9)
 
--- functions/kernel_matrix_dimension.m
+++ functions/kernel_matrix_dimension.m
@@ -3,7 +3,8 @@ function K = kernel_matrix_dimension(x, sigma_l, sigma_f)
 K = zeros(length(x));
 
 for i = 1:length(x)
-    for j = 1:length(x)
+    for j = i:length(x)
         K(i,j) = sigma_f*exp(-(x(i)-x(j))^2/(2*sigma_l));
+        K(j,i) = sigma_f*exp(-(x(i)-x(j))^2/(2*sigma_l));
     end
 end
