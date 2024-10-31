14007bb9c6d17f27e5b495d6c0888208cdcdce2c
Nackonator <nico@brunices.nl>
2024-10-31 18:44:27 +0100

    f

--- functions/GPregression_with_fitting.m
+++ functions/GPregression_with_fitting.m
@@ -28,10 +28,7 @@ function[GPmodel, predictions, mean, covariance_matrix, Kernel_train_data] = GPr
     % size_K_Xstar_Xstar  = size(K_Xstar_Xstar)
 
     % calculate mean vector and covariance matrix based on eq 2.19 from the book
-    %--+++====BACKSLASH OPERATOR ISNIET MOGELIJK OMDAT DE NUMBER OF ROWS NIET HETZELFDE ZIJN.====+++--
-    K_X_X_inv = inv(K_X_X); %pre-calculate the inverse seeing as it is needed in both the mean and covariance. This is also a very intensive inverse to calculate (big matrix).
-    %mean = K_X_X \ K_Xstar_X .* y;
-    %covariance_matrix = K_Xstar_Xstar - K_X_X \ K_Xstar_X * K_X_Xstar;
+    K_X_X_inv = inv(K_X_X); %pre-calculate the inverse seeing as it is needed in both the mean and covariance. Done seperately in order to time it.
     mean = K_Xstar_X * K_X_X_inv * y;
     covariance_matrix = K_Xstar_Xstar - K_Xstar_X*K_X_X_inv*K_X_Xstar;
 end
--- functions/kernel_matrix.m
+++ functions/kernel_matrix.m
@@ -1,15 +1,15 @@
 function Kernel = kernel_matrix(X, sigma_l, sigma_f)
     % METHOD1: non-optimized version, Is correct (I think)
-    N = size(X, 1);
-    Kernel = zeros(N, N);
-    for i = 1:N
-        for j = i:N
-            val = sigma_f * exp(-(norm(X(i,:) - X(j,:))^2)/(2*sigma_l));
-            Kernel(i,j) = val;
-            Kernel(j,i) = val;
-     
-        end
-    end
+    % N = size(X, 1);
+    % Kernel = zeros(N, N);
+    % for i = 1:N
+    %     for j = i:N
+    %         val = sigma_f * exp(-(norm(X(i,:) - X(j,:))^2)/(2*sigma_l));
+    %         Kernel(i,j) = val;
+    %         Kernel(j,i) = val;
+    % 
+    %     end
+    % end
 
     % % METHOD 2: optimized version of the for loop
     % N = size(X, 1);
@@ -24,7 +24,7 @@ function Kernel = kernel_matrix(X, sigma_l, sigma_f)
     % Kernel = squeeze(sigma_f .* exp(-0.5/sigma_l .* Kernel));
     
     % METHOD 3
-    % Kernel = sigma_f .* exp(-0.5/sigma_l .* pdist2(X, X).^2);
+    Kernel = sigma_f .* exp(-0.5/sigma_l .* pdist2(X, X).^2);
 
 end
 
